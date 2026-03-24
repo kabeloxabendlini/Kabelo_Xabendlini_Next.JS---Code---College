16821bf9c45e84c5ccb136449dffb43378c940db
kabelo385 <kabeloxabendlini385@gmail.com>
2026-03-24 21:39:27 +0200

    first commit

--- My_Next.JS_App/my-app2/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-// /app/components/LoadingPage.tsx
-const LoadingPage = () => (
-  <div className="flex items-center justify-center h-screen">
-    <span className="loading loading-spinner loading-xl"></span>
-  </div>
-);
-
-export default LoadingPage;
